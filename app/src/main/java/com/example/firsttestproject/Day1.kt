c43407615dbfa2e2d425e8e109a59b947d73208d
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-11-04 14:26:36 +0545

    Initial commit

---
+++ app/src/main/java/com/example/firsttestproject/Day1.kt
@@ -0,0 +1,22 @@
+package com.example.firsttestproject
+
+
+fun main(){
+
+
+
+    val age = arrayOf(10,20,30)
+
+    age[2]=50
+    println(age[2])
+
+
+    val age1=ArrayList<Int>()
+    val age2=arrayListOf<Int>(10,20,30)
+
+
+    age1.add(5)
+
+    age2.add(10)
+
+}
---
+++ app/src/main/java/com/example/firsttestproject/ui/theme/test.kt
@@ -0,0 +1,2 @@
+package com.example.firsttestproject.ui.theme
+
