fcd8d0fac43d5262ecff72ea3e2de585ca378d1a
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-11-04 14:40:12 +0545

    Array

--- app/src/main/java/com/example/firsttestproject/Day1.kt
+++ app/src/main/java/com/example/firsttestproject/Day1.kt
@@ -3,18 +3,19 @@ package com.example.firsttestproject
 
 fun main(){
 
+    var age = arrayOf(1,2,3)
+    println(age)
+    println("The first element of age is ${age[0]}")
+    println("The second element of age is ${age[1]}")
+    println("The second element of age is ${age[2]}")
 
 
-    val age = arrayOf(10,20,30)
+    println("***************************************")
 
-    age[2]=50
-    println(age[2])
-
-
-    val age1=ArrayList<Int>()
-
-
-    age1.add(5)
-
+    var name = arrayOf("Ram","Shyam","Hari")
+    name[1]="Sandis"
+    println("The first element of name is ${name[0]}")
+    println("The second element of name is ${name[1]}")
+    println("The Third element of name is ${name[2]}")
 
 }
